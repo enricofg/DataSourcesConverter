d9c2645b6030d0e65142ece1df310d095b88871b
Enrico Florentino Gomes <2181895@my.ipleiria.pt>
2020-11-26 17:19:50 +0000

    Row button click listener

--- DataSourcesConverter/FormDataSourcesConverter.Designer.cs
+++ DataSourcesConverter/FormDataSourcesConverter.Designer.cs
@@ -81,6 +81,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(564, 295);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Input
             // 
--- DataSourcesConverter/FormDataSourcesConverter.cs
+++ DataSourcesConverter/FormDataSourcesConverter.cs
@@ -12,10 +12,31 @@ namespace DataSourcesConverter
 {
     public partial class FormDataSourcesConverter : Form
     {
+
         public FormDataSourcesConverter()
         {
             InitializeComponent();
         }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+            DataGridView dg = dataGridView1;
+
+            ButtonAddConfig.Visible = true;
+
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+                e.RowIndex > 0)
+            {
+                //TODO - Button Clicked - Execute Code Here
+                //MessageBox.Show("Test"+e.RowIndex);
+                dg.Rows.RemoveAt(e.RowIndex);
+
+            }
+            else
+            {
+
+            }
+        }
     }
 }
