2a5d4e14f43661ba941c1ce0f312e3c68627107e
Enrico Gomes <2181895@my.ipleiria.pt>
2020-11-30 03:17:35 +0000

    Flow configuration handling almost complete

--- DataSourcesConverter/FormDataSourcesConverter.Designer.cs
+++ DataSourcesConverter/FormDataSourcesConverter.Designer.cs
@@ -28,18 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonRunFlow = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Input = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.PathInput = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Output = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.PathOutput = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonSaveFlow = new System.Windows.Forms.Button();
+            this.buttonLoadFlow = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonAddRow = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.ButtonAddConfig = new System.Windows.Forms.DataGridViewButtonColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.PathOutput = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Output = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.PathInput = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Input = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -58,33 +64,126 @@
             this.buttonRunFlow.BackColor = System.Drawing.Color.RoyalBlue;
             this.buttonRunFlow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonRunFlow.ForeColor = System.Drawing.Color.FloralWhite;
-            this.buttonRunFlow.Location = new System.Drawing.Point(589, 373);
+            this.buttonRunFlow.Location = new System.Drawing.Point(560, 373);
             this.buttonRunFlow.Name = "buttonRunFlow";
-            this.buttonRunFlow.Size = new System.Drawing.Size(112, 32);
+            this.buttonRunFlow.Size = new System.Drawing.Size(141, 32);
             this.buttonRunFlow.TabIndex = 2;
             this.buttonRunFlow.Text = "Run Flow";
             this.buttonRunFlow.UseVisualStyleBackColor = false;
             this.buttonRunFlow.Click += new System.EventHandler(this.buttonRunFlow_Click);
             // 
-            // dataGridView1
+            // dataGridView
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Input,
             this.PathInput,
             this.Output,
             this.PathOutput,
             this.ButtonAddConfig});
-            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonShadow;
-            this.dataGridView1.Location = new System.Drawing.Point(26, 63);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(675, 295);
-            this.dataGridView1.TabIndex = 3;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            this.dataGridView1.RowValidating += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_RowValidating);
+            this.dataGridView.GridColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dataGridView.Location = new System.Drawing.Point(26, 63);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(675, 295);
+            this.dataGridView.TabIndex = 3;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView.RowValidating += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_RowValidating);
+            // 
+            // buttonSaveFlow
+            // 
+            this.buttonSaveFlow.BackColor = System.Drawing.Color.ForestGreen;
+            this.buttonSaveFlow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSaveFlow.ForeColor = System.Drawing.Color.FloralWhite;
+            this.buttonSaveFlow.Location = new System.Drawing.Point(26, 373);
+            this.buttonSaveFlow.Name = "buttonSaveFlow";
+            this.buttonSaveFlow.Size = new System.Drawing.Size(73, 32);
+            this.buttonSaveFlow.TabIndex = 4;
+            this.buttonSaveFlow.Text = "Save Flow";
+            this.buttonSaveFlow.UseVisualStyleBackColor = false;
+            this.buttonSaveFlow.Click += new System.EventHandler(this.buttonSaveFlow_Click);
+            // 
+            // buttonLoadFlow
+            // 
+            this.buttonLoadFlow.BackColor = System.Drawing.Color.Gray;
+            this.buttonLoadFlow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonLoadFlow.ForeColor = System.Drawing.Color.FloralWhite;
+            this.buttonLoadFlow.Location = new System.Drawing.Point(105, 373);
+            this.buttonLoadFlow.Name = "buttonLoadFlow";
+            this.buttonLoadFlow.Size = new System.Drawing.Size(73, 32);
+            this.buttonLoadFlow.TabIndex = 5;
+            this.buttonLoadFlow.Text = "Load Flow";
+            this.buttonLoadFlow.UseVisualStyleBackColor = false;
+            this.buttonLoadFlow.Click += new System.EventHandler(this.buttonLoadFlow_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.Crimson;
+            this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonClear.ForeColor = System.Drawing.Color.FloralWhite;
+            this.buttonClear.Location = new System.Drawing.Point(184, 373);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(73, 32);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Clear Flow";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonAddRow
+            // 
+            this.buttonAddRow.BackColor = System.Drawing.Color.SeaGreen;
+            this.buttonAddRow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonAddRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddRow.ForeColor = System.Drawing.Color.FloralWhite;
+            this.buttonAddRow.Location = new System.Drawing.Point(676, 32);
+            this.buttonAddRow.Name = "buttonAddRow";
+            this.buttonAddRow.Size = new System.Drawing.Size(25, 25);
+            this.buttonAddRow.TabIndex = 7;
+            this.buttonAddRow.Text = "+";
+            this.buttonAddRow.UseVisualStyleBackColor = false;
+            this.buttonAddRow.Click += new System.EventHandler(this.buttonAddRow_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "dataflow_config";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "dataflow_config";
+            // 
+            // ButtonAddConfig
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.NullValue = "Remove";
+            this.ButtonAddConfig.DefaultCellStyle = dataGridViewCellStyle3;
+            this.ButtonAddConfig.HeaderText = "Config";
+            this.ButtonAddConfig.Name = "ButtonAddConfig";
+            this.ButtonAddConfig.Text = "";
+            // 
+            // PathOutput
+            // 
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.CornflowerBlue;
+            this.PathOutput.DefaultCellStyle = dataGridViewCellStyle2;
+            this.PathOutput.HeaderText = "Output: Path+Filename";
+            this.PathOutput.Name = "PathOutput";
+            // 
+            // Output
+            // 
+            this.Output.HeaderText = "Output";
+            this.Output.Items.AddRange(new object[] {
+            "HTML Page",
+            "RESTful API"});
+            this.Output.Name = "Output";
+            // 
+            // PathInput
+            // 
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.CornflowerBlue;
+            this.PathInput.DefaultCellStyle = dataGridViewCellStyle1;
+            this.PathInput.HeaderText = "File Path / URL / IP";
+            this.PathInput.Name = "PathInput";
             // 
             // Input
             // 
@@ -97,50 +196,23 @@
             "Broker"});
             this.Input.Name = "Input";
             // 
-            // PathInput
-            // 
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.CornflowerBlue;
-            this.PathInput.DefaultCellStyle = dataGridViewCellStyle1;
-            this.PathInput.HeaderText = "File Path / URL / IP";
-            this.PathInput.Name = "PathInput";
-            // 
-            // Output
-            // 
-            this.Output.HeaderText = "Output";
-            this.Output.Items.AddRange(new object[] {
-            "HTML Page",
-            "RESTful API"});
-            this.Output.Name = "Output";
-            // 
-            // PathOutput
-            // 
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.CornflowerBlue;
-            this.PathOutput.DefaultCellStyle = dataGridViewCellStyle2;
-            this.PathOutput.HeaderText = "Output: Path+Filename";
-            this.PathOutput.Name = "PathOutput";
-            // 
-            // ButtonAddConfig
-            // 
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle3.NullValue = "Remove";
-            this.ButtonAddConfig.DefaultCellStyle = dataGridViewCellStyle3;
-            this.ButtonAddConfig.HeaderText = "Config";
-            this.ButtonAddConfig.Name = "ButtonAddConfig";
-            this.ButtonAddConfig.Text = "";
-            // 
             // FormDataSourcesConverter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(725, 428);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonAddRow);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonLoadFlow);
+            this.Controls.Add(this.buttonSaveFlow);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.buttonRunFlow);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormDataSourcesConverter";
             this.Text = "Data Sources Converter";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,7 +222,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonRunFlow;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button buttonSaveFlow;
+        private System.Windows.Forms.Button buttonLoadFlow;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonAddRow;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.DataGridViewComboBoxColumn Input;
         private System.Windows.Forms.DataGridViewTextBoxColumn PathInput;
         private System.Windows.Forms.DataGridViewComboBoxColumn Output;
--- DataSourcesConverter/FormDataSourcesConverter.cs
+++ DataSourcesConverter/FormDataSourcesConverter.cs
@@ -30,7 +30,7 @@ namespace DataSourcesConverter
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             var senderGrid = (DataGridView)sender;
-            DataGridView dg = dataGridView1;
+            DataGridView dg = dataGridView;
 
             if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
                 e.RowIndex >= 0 && 
@@ -43,11 +43,11 @@ namespace DataSourcesConverter
 
         private void buttonRunFlow_Click(object sender, EventArgs e)
         {
-            DataGridView dg = dataGridView1;
+            DataGridView dg = dataGridView;
 
             foreach (DataGridViewRow row in dg.Rows)
             {
-                if (row.IsNewRow == false)
+                if (row.IsNewRow == false && row.Cells[0].Value != null)
                 {
                     string inputOption = row.Cells["Input"].Value.ToString();
                     string inputPath = row.Cells["PathInput"].Value.ToString();
@@ -67,6 +67,147 @@ namespace DataSourcesConverter
             }
         }
 
+        private void dataGridView1_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
+        {            
+            DataGridViewRow row = dataGridView.Rows[e.RowIndex];
+            DataGridViewCell inputCell = row.Cells["Input"];
+            DataGridViewCell inputPathCell = row.Cells["PathInput"];
+            DataGridViewCell outputCell = row.Cells["Output"];
+            DataGridViewCell outputPathCell = row.Cells["PathOutput"];
+            e.Cancel = ValidateCells(inputCell, inputPathCell, outputCell, outputPathCell);
+        }
+
+        private void buttonAddRow_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Add();
+        }
+
+        #region Flow Configuration Controls
+        private void buttonSaveFlow_Click(object sender, EventArgs e)
+        {
+            DataGridView dg = dataGridView;
+            string file = "";
+
+            saveFileDialog1.InitialDirectory = Application.StartupPath;
+            saveFileDialog1.Filter = "xml files (*.bin)|*.bin";
+
+            //se o utilizador selecionou o botão "OK"
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                file = saveFileDialog1.FileName;
+            }
+            
+            if (dataGridView.Rows.Count != 0 && file != "")
+            {
+                try
+                {
+                    //source: https://stackoverflow.com/questions/2952161/c-sharp-saving-a-datagridview-to-file-and-loading
+                    using (BinaryWriter bw = new BinaryWriter(File.Open(file, FileMode.Create)))
+                    {
+                        bw.Write(dg.Columns.Count);
+                        bw.Write(dg.Rows.Count);
+                        foreach (DataGridViewRow row in dg.Rows)
+                        {
+                            if (row.IsNewRow == false) ///TODO: only save valid cells
+                            {
+                                for (int j = 0; j < dg.Columns.Count; ++j)
+                                {
+                                    object val = row.Cells[j].Value;
+                                    if (val == null)
+                                    {
+                                        bw.Write(false);
+                                        bw.Write(false);
+                                    }
+                                    else
+                                    {
+                                        bw.Write(true);
+                                        bw.Write(val.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Flow configuration saved!");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error saving flow configuration!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Cannot save configuration! \nThe file must have a name and the data flow must not be empty.");
+            }
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView.DataSource = null;
+                dataGridView.Rows.Clear();
+                dataGridView.Refresh();    
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error clearing flow!");
+            }
+        }
+
+        private void buttonLoadFlow_Click(object sender, EventArgs e)
+        {
+            DataGridView dg = dataGridView;
+            dg.Rows.Clear();
+            string file = "";
+
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Filter = "xml files (*.bin)|*.bin";
+
+            //se o utilizador selecionou o botão "OK"
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                file = openFileDialog1.FileName;
+            }
+            
+            try
+            {
+                using (BinaryReader bw = new BinaryReader(File.Open(file, FileMode.Open)))
+                {
+                    int n = bw.ReadInt32();
+                    int m = bw.ReadInt32();
+                    for (int i = 0; i < m; ++i)
+                    {
+                        dg.Rows.Add();
+                        for (int j = 0; j < n; ++j)
+                        {
+                            if (bw.ReadBoolean())
+                            {
+                                dg.Rows[i].Cells[j].Value = bw.ReadString();
+                            }
+                            else bw.ReadBoolean();
+                        }
+                    }
+                }
+
+                MessageBox.Show("Data flow configuration loaded!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error loading flow configuration!");
+            }
+        }
+        #endregion
+
+        private Boolean ValidateCells(DataGridViewCell inputCell, DataGridViewCell inputPathCell, DataGridViewCell outputCell, DataGridViewCell outputPathCell)
+        {
+            if (validInputs.Contains(inputCell.Value) && validOutputs.Contains(outputCell.Value) && inputPathCell.Value != null && outputPathCell.Value != null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
         private static void RunFlowRowItemOptions(string inputOption, string inputPath, string outputOption, string outputPath)
         {
             if (inputOption == C_EXCEL_FILE)
@@ -74,15 +215,15 @@ namespace DataSourcesConverter
                 try
                 {
                     string readExcel = Excel_Lib.ExcelHandler.ReadFromExcelFile(inputPath);
-
-                    MessageBox.Show("The define output path is " + outputPath + " and the read excel message is:\n" + readExcel);
-                    WriteOutput(outputOption, outputPath, readExcel);                    
+                    //MessageBox.Show("The define output path is " + outputPath + " and the read excel message is:\n" + readExcel);
+                    WriteOutput(outputOption, outputPath, readExcel);
                 }
                 catch (Exception)
                 {
                     MessageBox.Show("File at path \"" + inputPath + "\" not found!");
                 }
-            } else if(inputOption == C_XML_FILE)
+            }
+            else if (inputOption == C_XML_FILE)
             {
                 try
                 {
@@ -90,7 +231,7 @@ namespace DataSourcesConverter
                     List<string> infoXML = XMLHandler.GetXMLInfo();
                     string outputXML = "";
 
-                    foreach(string item in infoXML)
+                    foreach (string item in infoXML)
                     {
                         outputXML += item;
                     }
@@ -108,14 +249,14 @@ namespace DataSourcesConverter
         {
             try
             {
-                if(outputOption == C_HTML_PAGE)
+                if (outputOption == C_HTML_PAGE)
                 {
                     //Pass the filepath and filename to the StreamWriter Constructor
                     StreamWriter sw = new StreamWriter(outputPath + ".html");
                     //Write a line of text
                     sw.WriteLine(readInfo);
                     //Close the file
-                    MessageBox.Show("HTML File "+ outputPath + ".html created!");
+                    MessageBox.Show("HTML File " + outputPath + ".html created!");
                     sw.Close();
                 }
             }
@@ -127,47 +268,6 @@ namespace DataSourcesConverter
             {
                 Console.WriteLine("Executing finally block.");
             }
-        }
-
-        private void dataGridView1_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
-        {            
-            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
-
-            /*disable clicking of the remove button from other cells 
-            foreach (DataGridViewRow item in dataGridView1.Rows)
-            {
-                if(!item.IsNewRow)
-                {
-                    item.DefaultCellStyle.BackColor = Color.LightGray; 
-                }
-            }*/
-
-            DataGridViewCell inputCell = row.Cells["Input"];
-            DataGridViewCell inputPathCell = row.Cells["PathInput"];
-            DataGridViewCell outputCell = row.Cells["Output"];
-            DataGridViewCell outputPathCell = row.Cells["PathOutput"];
-            e.Cancel = ValidateCells(inputCell, inputPathCell, outputCell, outputPathCell);
-        }
-
-        private Boolean ValidateCells(DataGridViewCell inputCell, DataGridViewCell inputPathCell, DataGridViewCell outputCell, DataGridViewCell outputPathCell)
-        {           
-            if (validInputs.Contains(inputCell.Value) && validOutputs.Contains(outputCell.Value) && inputPathCell.Value != null && outputPathCell.Value != null)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private void dataGridView1_RowValidated(object sender, DataGridViewCellEventArgs e)
-        {
-            //MessageBox.Show("Data source configuration added!");
-            /*DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
-            row.Cells["ButtonAddConfig"].Value="Remove";*/
-            /*foreach (DataGridViewRow item in dataGridView1.Rows)
-            {
-                item.DefaultCellStyle.BackColor = Color.White;                
-            }*/
         }
     }
 }
